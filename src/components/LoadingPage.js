ef95b6ae8af9bd8a8efa5aa1aaa601db8940d1ad
BilalHussain009 <bilalhussain.work@gmail.com>
2019-07-31 17:56:52 +0500

    Finished App / Fully Styled

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const LoadingPage=()=>(
+    <div className='loader'>
+        <img className='loader__image' src='/images/bars.svg'/>
+    </div>
+);
+export default LoadingPage;
